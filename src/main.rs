36241ac2cb4be361dc697975e416b22c4eab5a26
rb bobr <dannobarret@gmail.com>
2024-11-27 10:15:18 +0300

    project had been generated

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+
+// Print panic message to probe console
+use panic_probe as _;
+
+
+use cortex_m_rt::entry;
+use stm32f4xx_hal::{
+    pac,
+    prelude::*,
+};
+
+// use embassy_hal_internal::{Peripheral, PeripheralRef};
+
+
+#[allow(clippy::empty_loop)]
+#[entry]
+fn main() -> ! {
+    
+    loop {
+        
+    }
+}
